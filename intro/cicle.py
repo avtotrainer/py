7783fd554ce3f537f22a3fe441a9d3900070ecd2
avtotrainer <trainer@din.ge>
2023-11-17 23:35:07 +0400

    დავამატე ფაილები

---
+++ intro/cicle.py
@@ -0,0 +1,9 @@
+# ᲪᲘᲙᲚᲘ for
+for i in range(5):
+    print(i)
+
+# ᲪᲘᲙᲚᲘ while
+counter = 0
+while counter < 5:
+    print(counter)
+    counter += 1
---
+++ intro/istr.py
@@ -0,0 +1,10 @@
+# ᲡᲢᲠᲘᲥᲝᲜᲔᲑᲘᲡ ᲒᲐᲔᲠᲗᲘᲐᲜᲔᲑᲐ
+str1 = "Hello"
+str2 = "World"
+result = str1 + " " + str2
+print(result)
+
+# ᲡᲢᲠᲘᲥᲝᲜᲔᲑᲘᲡ ᲤᲝᲠᲛᲐᲢᲘᲠᲔᲑᲐ
+name = "ᲡᲝᲤᲘᲐ"
+age = 25
+print("ᲒᲐᲛᲐᲠᲯᲝᲑᲐ, ᲛᲔ ᲛᲥᲕᲘᲐ {} ᲓᲐ ᲛᲔ ᲕᲐᲠ {} ᲬᲚᲘᲡ.".format(name, age))
